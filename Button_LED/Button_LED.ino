ee1213f862e31fa7757d81d0ec38128aae8898fb
Kevin Lindstrom <kevin.lindstrom.ct@gmail.com>
2020-03-07 10:00:08 -0500

    some new code

---
+++ Button_LED/Button_LED.ino
@@ -0,0 +1,25 @@
+// constants won't change. They're used here to set pin numbers:
+const int buttonPin = 2;     // the number of the pushbutton pin
+const int ledPin =  13;      // the number of the LED pin
+
+// variables will change:
+int buttonState = 0;         // variable for reading the pushbutton status
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ledPin, OUTPUT);      //  Declares that the LED is an ouput
+}
+
+void loop() {
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {
+    // turn LED on:
+    digitalWrite(ledPin, HIGH);
+  } else {
+    // turn LED off:
+    digitalWrite(ledPin, LOW);
+  }
+}
