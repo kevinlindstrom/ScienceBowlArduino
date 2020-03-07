f986be37e818cd79c622c83f3d05b27fc003a57b
Kevin Lindstrom <kevin.lindstrom.ct@gmail.com>
2020-03-07 13:09:42 -0500

    final code

--- potentiometer/potentiometer.ino
+++ potentiometer/potentiometer.ino
@@ -1,8 +1,8 @@
+int poten = A0
 // the setup routine runs once when you press reset:
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
-  pinMode(Ana
 }
 
 // the loop routine runs over and over again forever:
